a7f3f727cdde8b37e2dde84302b913b43812b446
gasvel <gastonveliez95@gmail.com>
2019-04-27 16:28:07 -0300

    Componente de Toolbox con seleccion de maquinas creado (No incluido en vista principal)

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,7 +4,9 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import Toolbox from '../controllers/Toolbox';
+
+  import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -17,3 +19,10 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+storiesOf('Toolbox',module)
+  .add('default',() => (
+    <Toolbox></Toolbox>
+  ));
+
+  require('../controllers/Toolbox');
