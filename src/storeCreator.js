dd63c5b9d574b0f1701eb5552f7c1773a16114b7
Matias Cavallin <matiascavallin96@gmail.com>
2019-05-17 20:44:04 -0300

    Arreglos de mover (creo que no explota mas)

--- src/storeCreator.js
+++ src/storeCreator.js
@@ -3,7 +3,7 @@ import reducers from './reducers';
 export default function() {
  return createStore(
      reducers,
-     {maquinas:{tablero:[],maquinaSeleccionada:'NO',herramienta: "SELECCIONAR",orientacionSeleccionada:"NO", materialSeleccionado: "NO", dinero: 1000}},
+     {maquinas:{tablero:[],maquinaSeleccionada:'NO',herramienta: "SELECCIONAR",orientacionSeleccionada:"NO", materialSeleccionado: "NO", dinero: 1000, maquinaSeleccionadaMover: "NO", materialSeleccionadoMover: "NO"}},
      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
      )
 };
